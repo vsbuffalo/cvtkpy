Fixed a serious bug and added standard deviation based CIs
Previous versions to cvtk had a bug where specifiying sequence IDs
with keep_seqis in calc_cov_by_group() where leading the wrong
loci to be used. This was wrong cause of severe bias in the CIs.

Also, I have added a standard CI procedure; for cases where
genome-wide covariances are biased compared to boostrap-based
covariances, this can correct them.

cvtk/bootstrap.py
@@ -1,5 +1,6 @@
 from itertools import chain
 import numpy as np
+from scipy.stats import norm
 from tqdm import tnrange
 from cvtk.utils import view_along_axis
 from cvtk.cov import stack_temporal_covariances, temporal_replicate_cov
@@ -20,6 +21,11 @@ def bootstrap_ci(estimate, straps, alpha=0.05, method='pivot', axis=0, stack=Tru
         CIs = qlower, estimate, qupper
     elif method == 'pivot':
         CIs = 2*estimate - qupper, estimate, 2*estimate - qlower
+    elif method == 'standard':
+        # the standard method, see Efron 2016, p. 186
+        sd = np.std(straps, axis=axis)
+        z = norm.ppf(1-alpha/2)
+        CIs = estimate - z*sd, estimate, estimate + z*sd
     else:
         raise ValueError("method must be either 'pivot' or 'percentile'")
     if stack:
@@ -41,9 +47,9 @@ def weighted_mean(array, weights, axis=0):
 
 
 def block_bootstrap_ratio_averages(blocks_numerator, blocks_denominator,
-                                   block_indices, block_seqids, B, estimator=np.divide,
+                                   block_indices, B, estimator=np.divide,
                                    statistic=None,
-                                   alpha=0.05, keep_seqids=None, return_straps=False,
+                                   alpha=0.05, return_straps=False,
                                    ci_method='pivot', progress_bar=False, **kwargs):
     """
     This block bootstrap is used often for quantities we need to calculate that are
@@ -64,17 +70,11 @@ def block_bootstrap_ratio_averages(blocks_numerator, blocks_denominator,
     else:
         B_range = range(int(B))
 
-    # We create the vector of indices to sample with replacement from, excluding
-    # any blocks with seqids not in keep_seqids.
-    if keep_seqids is not None:
-        blocks = np.array([i for i, seqid in enumerate(block_seqids)
-                           if seqid in keep_seqids], dtype='uint32')
-    else:
-        blocks = np.array([i for i, seqid in enumerate(block_seqids)], dtype='uint32')
+    assert(len(blocks_numerator) == len(blocks_denominator))
+    blocks = np.arange(len(blocks_numerator), dtype='uint32')
 
     # Calculate the weights
     weights = np.array([len(x) for x in block_indices])
-    weights = weights/weights.sum()
 
     # number of samples in resample
     nblocks = len(blocks)
@@ -90,7 +90,7 @@ def block_bootstrap_ratio_averages(blocks_numerator, blocks_denominator,
     if That is None:
         That = np.mean(straps, axis=0)
     if return_straps:
-        return straps
+        return straps, weights
     return bootstrap_ci(That, straps, alpha=alpha, method=ci_method)
 
 
@@ -112,6 +112,7 @@ def block_bootstrap(freqs,
                     ci_method='pivot', progress_bar=False, **kwargs):
     """
     """
+    assert(False) # deprciated
     if progress_bar:
         B_range = tnrange(int(B), desc="bootstraps")
     else:
@@ -139,7 +140,6 @@ def block_bootstrap(freqs,
         stat = estimator(freqs[..., indices], **kwargs)
         straps.append(stat)
     straps = np.stack(straps)
-    That = np.mean(straps, axis=0)
     if return_straps:
         return straps
     return bootstrap_ci(That, straps, alpha=alpha, method=ci_method)

cvtk/cov.py
@@ -221,6 +221,7 @@ def cov_by_group(groups, freqs, depths=None, diploids=None, standardize=True,
                                      share_first=share_first,
                                      return_ratio_parts=return_ratio_parts,
                                      deltas=group_deltas)
+        # __import__('pdb').set_trace()
         if return_ratio_parts:
             cov, het_denom = res
             covs.append(cov)
@@ -301,7 +302,7 @@ def temporal_replicate_cov(freqs, depths=None, diploids=None,
         assert(depths.shape == (R, T+1, L))
     if diploids is not None:
         assert(diploids.shape == (R, T+1, 1))
-
+    # __import__('pdb').set_trace()
     # calculate variance-covariance matrix
     if use_masked:
         deltas_masked = np.ma.masked_invalid(deltas)

cvtk/cvtk.py
@@ -129,24 +129,25 @@ class TemporalFreqs(object):
         return convergence_corr_by_group(covs, self.R, self.T)
 
 
-    def calc_cov_by_group(self, groups, keep_seqids=None, bias_correction=True,
-                           standardize=True, use_masked=False, return_ratio_parts=False,
-                           progress_bar=False):
+    def calc_cov_by_group(self, groups, group_seqids=None, keep_seqids=None,
+                          bias_correction=True, standardize=True,
+                          use_masked=False, return_ratio_parts=False,
+                          progress_bar=False):
         """
         Calculate covariances, grouping loci by the indices in groups.
         """
         freqs, depths = self.freqs, self.depths
         if keep_seqids is not None:
-            assert(isinstance(keep_seqids, (tuple, list)))
-            idx = np.array([i for i, seqid in enumerate(self.gintervals.seqid)
-                            if seqid in keep_seqids])
-            freqs, depths = freqs[..., idx], depths[..., idx]
+            assert(group_seqids is not None)
+            # thin out the groups that don't have the right seqid
+            groups = [g for seqid, g in zip(group_seqids, groups) if
+                      seqid in keep_seqids]
         res = cov_by_group(groups, freqs,
-                            depths=depths, diploids=self.diploids,
-                            standardize=standardize,
-                            use_masked=use_masked, share_first=self.share_first,
-                            return_ratio_parts=return_ratio_parts,
-                            bias_correction=bias_correction, progress_bar=progress_bar)
+                           depths=depths, diploids=self.diploids,
+                           standardize=standardize,
+                           use_masked=use_masked, share_first=self.share_first,
+                           return_ratio_parts=return_ratio_parts,
+                           bias_correction=bias_correction, progress_bar=progress_bar)
         if return_ratio_parts:
             covs, het_denoms = res
             return covs, het_denoms
@@ -231,7 +232,8 @@ class TiledTemporalFreqs(TemporalFreqs):
 
     def calc_cov_by_tile(self, *args, **kwargs):
         indices = self.tile_indices
-        return super().calc_cov_by_group(indices, *args, **kwargs)
+        return super().calc_cov_by_group(indices, group_seqids=self.tiles.seqid,
+                                         *args, **kwargs)
 
     def calc_var_by_tile(self, *args, **kwargs):
         indices = self.tile_indices
@@ -299,6 +301,7 @@ class TiledTemporalFreqs(TemporalFreqs):
 
     def bootstrap_cov(self, B, alpha=0.05, keep_seqids=None, progress_bar=False,
                       average_replicates=True, return_straps=False, ci_method='pivot',
+                      use_bs_estimate=False,
                       **kwargs):
         """
         Bootstrap whole covaraince matrix.
@@ -315,18 +318,20 @@ class TiledTemporalFreqs(TemporalFreqs):
         cov = self.calc_cov(keep_seqids=keep_seqids, **kwargs)
         if average_replicates:
             cov = np.nanmean(stack_temporal_covariances(cov, self.R, self.T), axis=2)
+        # the filter of seqids is done at this step
         covs, het_denoms = self.calc_cov_by_tile(return_ratio_parts=True,
                                                  keep_seqids=keep_seqids, **kwargs)
         covs, het_denoms = np.stack(covs), np.stack(het_denoms)
+        if use_bs_estimate:
+            cov = None
         return block_bootstrap_ratio_averages(covs, het_denoms,
                                               block_indices=self.tile_indices,
-                                              block_seqids=self.tile_df['seqid'].values,
                                               estimator=cov_estimator,
+                                              ci_method=ci_method,
                                               statistic=cov,
                                               B=B, alpha=alpha,
                                               progress_bar=progress_bar,
-                                              keep_seqids=keep_seqids, return_straps=return_straps,
-                                              ci_method=ci_method,
+                                              return_straps=return_straps,
                                               # kwargs passed directly to cov_estimator
                                               average_replicates=average_replicates,
                                               R=self.R, T=self.T)